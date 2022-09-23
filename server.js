7f7962f26d9b49deca8974276bf4920b64a57d3f
Prashant Rawat <prashantxrawat@gmail.com>
2022-09-23 11:29:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-const { createServer } = require("http");
-const next = require("next");
+const express = require('express')
+const next = require('next')
 
-const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.get('/p/:id', (req, res) => {
+    const actualPage = '/post'
+    const queryParams = { title: req.params.id }
+    app.render(req, res, actualPage, queryParams)
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:' + port)
+  })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
